b0469b7d6dcbbfbd00ad69cb0ccac3034f61f70c
justin <jgoodhcg@gmail.com>
2021-03-08 08:14:29 -0500

    enhancement(#4): Get page title fetch working locally -- again

--- backend/src/backend/main.cljs
+++ backend/src/backend/main.cljs
@@ -7,7 +7,11 @@
 
 (defn handler [event context callback]
   (go
-    (let [maybe-path (<p! (-> chromium (j/get :executablePath)))
+    (let [{:keys [graph
+                  email
+                  password]} event
+
+          maybe-path (<p! (-> chromium (j/get :executablePath)))
           puppeteer  (-> chromium (j/get :puppeteer))
           browser    (<p! (-> puppeteer
                               (j/call
@@ -23,23 +27,50 @@
                                     (when maybe-path {:executablePath maybe-path}))))))
           page       (<p! (-> browser (j/call :newPage)))]
       (try
-        (<p! (-> page (j/call :goto "https://jgood.io")))
-        (callback
-          nil
-          (clj->js
-            {:statusCode 200
-             :body
-             (js/JSON.stringify
-               (clj->js {:event   event
-                         :context context
-                         :result  (<p! (-> page (j/call :title))) }))}))
-        (catch js/Error err (js/console.log (ex-cause err))))
+        (-> page (j/call :setDefaultTimeout 60000))
+        (<p! (-> page (j/call :goto (str "https://roamresearch.com/#/app/" graph))))
+        (tap> "made it to the graph")
+        (<p! (-> page (j/call :waitForNavigation)))
+        (tap> "waited for navigation")
+        (<p! (-> page (j/call :waitForSelector "input[name=email]")))
+        (tap> "waited for selector")
+        (<p! (-> page (j/call :type "input[name=email]" email)))
+        (tap> "typed in email")
+        (<p! (-> page (j/call :type "input[name=password]" password)))
+        (tap> "typed in password")
+        (<p! (-> page (j/call :click ".bp3-button")))
+        (tap> "clicked login")
+        (<p! (-> page (j/call :waitForSelector ".bp3-icon-more")))
+        (tap> "waited for load up")
+        (let [titles (<p! (-> page (j/call :evaluate
+                                           (fn [q]
+                                             (js/Promise.resolve
+                                               (.q (.-roamAlphaAPI js/window) q)))
+                                           "[:find ?n :where [?e :node/title ?n]]")))]
+
+          (tap> "ran query")
+          (callback
+            nil
+            (clj->js
+              {:statusCode 200
+               :body
+               (js/JSON.stringify
+                 (clj->js {:event   event
+                           :context context
+                           :result  titles}))})))
+        (catch js/Error err
+          (tap> err)
+          (js/console.log (ex-cause err))))
       (.close browser))))
 
 (comment
 
   ;; query [:find ?n :where [?e :node/title ?n]]
 
-  (handler nil nil #(tap> %2))
+  (handler {:email    "xxx"
+            :graph    "xxx"
+            :password "xxx"}
+           nil
+           #(tap> %2))
 
   )
